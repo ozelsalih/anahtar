3f47f211f1aca9f72f1db7bcf36151e76dc6b2d6
ozelsalih <42327890+ozelsalih@users.noreply.github.com>
2021-02-27 11:32:59 +0300

    Flask secret key is now generated randomly

--- app/app.py
+++ app/app.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import random, hashlib, string
+from flask import Flask, render_template, request
 
-@app.route('/')
+SECRET_KEY = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
+HASHED_SECRET_KEY = hashlib.sha256()
+HASHED_SECRET_KEY.update(bytes(SECRET_KEY, encoding='utf-8'))
+SECRET_KEY = HASHED_SECRET_KEY.digest()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+hashed_password = hashlib.sha256()
+hashed_password.update(b"This is a")
+hashed_password.update(b" great python tutorial.")
+hashed_password.digest()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
